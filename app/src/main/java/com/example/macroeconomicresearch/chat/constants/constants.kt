a2b61ca250a47bc5d497d5dfadf4278589a8d758
sahithi-kalakonda <sahithikalakonda145@gmail.com>
2024-03-10 19:39:35 -0700

    Commit changes

---
+++ app/src/main/java/com/example/macroeconomicresearch/chat/constants/constants.kt
@@ -0,0 +1,6 @@
+package com.example.macroeconomicresearch.chat.constants
+
+const val ApiKey = "sec_wsLeEgvivzH9dbIyuX9LC1HIUaEX4z26"
+const val SourceId = "cha_G54eTtRSIvA8rbSZXJbNj"
+const val urlToImageAppIcon = "https://res.cloudinary.com/apideck/image/upload/v1672442492/marketplaces/ckhg56iu1mkpc0b66vj7fsj3o/listings/-4-ans_frontend_assets.images.poe.app_icon.png-26-8aa0a2e5f237894d_tbragv.png"
+const val urlToAvatarGPT = "https://gptapk.com/wp-content/uploads/2023/02/chatgpt-icon.png"
---
+++ app/src/main/java/com/example/macroeconomicresearch/chat/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.macroeconomicresearch.chat.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF03A9F4)
+val PurpleGrey80 = Color(0xFF2196F3)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF03A9F4)
+val PurpleGrey40 = Color(0xFF2196F3)
+val Pink40 = Color(0xFF03A9F4)
